d922abb1e5a0509eed9769f4dcce35404e6b8e40
isabellacpmelo <isacpmelo@gmail.com>
2023-04-16 22:20:04 -0300

    feat: Integrating the "SWAPI" API into the project #3

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,11 @@
 <script>
 export default {
   name: 'IndexPage',
+  // criar chamada fetch para a api https://swapi.dev/api/
+  async fetch() {
+    const res = await fetch('https://swapi.dev/api/')
+    const data = await res.json()
+    return { data }
+  },
 }
 </script>
