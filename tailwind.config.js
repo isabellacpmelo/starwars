6e33866aabf23b21d544f26dcdae15f6fa3e2386
isabellacpmelo <isacpmelo@gmail.com>
2023-04-16 21:25:34 -0300

    feat: Installing tailwind css #1

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  darkMode: false,
+  variants: {
+    extend: {},
+  },
+}
+
