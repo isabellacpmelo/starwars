323f3a90281281a7dfca5b63b80aab701d2deffd
isabellacpmelo <isacpmelo@gmail.com>
2023-04-17 20:37:06 -0300

    fix: Rearranging data to get all people from star wars api. Fixed #3

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,42 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>Star Wars</h1>
+    <ul>
+      <li v-for="person in people" :key="person.name">
+        {{ person.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
-  // criar chamada fetch para a api https://swapi.dev/api/
+  data() {
+    return {
+      allData: [],
+    }
+  },
   async fetch() {
-    const res = await fetch('https://swapi.dev/api/')
-    const data = await res.json()
-    return { data }
+    await this.fetchData('https://swapi.dev/api/people')
+  },
+  computed: {
+    people() {
+      const people = this.allData
+      return people
+    },
+  },
+  methods: {
+    async fetchData(url) {
+      const res = await fetch(url)
+      const data = await res.json()
+      this.allData = this.allData.concat(data.results)
+
+      if (data.next) {
+        // if there is a next page, fetch it
+        await this.fetchData(data.next)
+      }
+    },
   },
 }
 </script>
