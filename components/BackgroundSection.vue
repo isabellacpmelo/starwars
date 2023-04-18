24a228bd8f2be668bdde2b007a548bc82b1129e5
isabellacpmelo <isacpmelo@gmail.com>
2023-04-17 21:43:14 -0300

    feat: creating a background component

---
+++ components/BackgroundSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <section :class="`w-full ${bgClass} ${py} ${px}`">
+    <div :class="`${containerClass} mx-auto`">
+      <slot name="content" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    bgClass: {
+      type: String,
+      default: 'bg-white',
+    },
+    py: {
+      type: String,
+      default: 'py-16',
+    },
+    px: {
+      type: String,
+      default: 'px-6',
+    },
+    containerClass: {
+      type: String,
+      default: 'container',
+    },
+  },
+}
+</script>
