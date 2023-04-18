87cf21b367823244419b53f5a777975280bdf24e
isabellacpmelo <isacpmelo@gmail.com>
2023-04-17 21:48:57 -0300

    feat: Adding the BackgroundSection component into index.vue #4

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,33 @@
 <template>
-  <div>
+  <section>
+    <BackgroundSection bg-class="bg-hero-pattern" class="bg-cover min-h-screen">
+      <template #content>
+        <div class="justify-between flex flex-col fit-submenu">
+          <div
+            class="pt-8 lg:pt-12 xl:pt-36 pb-6 lg:pb-0 my-6 lg:mt-0 lg:mb-0 border-b border-solid border-gray-700 border-opacity-20"
+          >
+            <h1 class="text-7xl font-medium text-white">Star Wars</h1>
+          </div>
+        </div>
+      </template>
+    </BackgroundSection>
     <h1>Star Wars</h1>
     <ul>
       <li v-for="person in people" :key="person.name">
         {{ person.name }}
       </li>
     </ul>
-  </div>
+  </section>
 </template>
 
 <script>
+import BackgroundSection from '../components/BackgroundSection.vue'
+
 export default {
   name: 'IndexPage',
+  components: {
+    BackgroundSection,
+  },
   data() {
     return {
       allData: [],
