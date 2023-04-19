b2e0e937a40a8dcb4824292ed8eaf2a51f71f906
isabellacpmelo <isacpmelo@gmail.com>
2023-04-19 15:07:24 -0300

    fix: organizing code to make it faster

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,6 +1,9 @@
 <template>
   <section>
-    <hero-section hero-image="bg-hero-pattern" :title="name">
+    <hero-section
+      hero-image="bg-hero-pattern"
+      :title="name.toLocaleLowerCase()"
+    >
       <template #heroContent>
         <div class="flex flex-col md:flex-row items-center justify-between">
           <nuxt-link to="/">
@@ -18,7 +21,7 @@
               class="w-full h-auto rounded-lg border border-gray-900"
             />
           </div>
-          <div class="w-full md:w-1/2 px-8">
+          <div class="w-full md:w-1/2 px-8 mt-4 md:mt-0">
             <ul class="text-banana-yellow font-mono text-lg md:text-2xl">
               <li><span class="font-extrabold">Name:</span> {{ name }}</li>
               <li><span class="font-extrabold">Height: </span> {{ height }}</li>
@@ -41,18 +44,19 @@
               </li>
               <li>
                 <span class="font-extrabold">Films: </span
-                >{{ films.length ? films.join(', ') : films }}
+                >{{ replaceMovies(films.toString()) }}
               </li>
               <li>
-                <span class="font-extrabold">Species: </span>{{ species }}
+                <span class="font-extrabold">Species: </span
+                >{{ replaceMovies(species.toString()) }}
               </li>
               <li>
                 <span class="font-extrabold">Vehicles: </span
-                >{{ vehicles.length ? vehicles.join(', ') : vehicles }}
+                >{{ replaceMovies(vehicles.toString()) }}
               </li>
               <li>
                 <span class="font-extrabold">Starships: </span
-                >{{ starships.length ? starships.join(', ') : starships }}
+                >{{ replaceMovies(starships.toString()) }}
               </li>
             </ul>
           </div>
@@ -96,77 +100,76 @@ export default {
     },
   },
   async mounted() {
-    await this.fetchData(`https://swapi.dev/api/people/?search=${this.slug}`)
+    try {
+      this.loading = true
+      const res = await fetch(
+        `https://swapi.dev/api/people/?search=${this.slug}`
+      )
+      const data = await res.json()
+      const person = data.results[0]
+      this.name = person.name
+      this.height = person.height
+      this.mass = person.mass
+      this.hairColor = person.hair_color
+      this.skinColor = person.skin_color
+      this.eyeColor = person.eye_color
+      this.birthYear = person.birth_year
+      this.gender = person.gender
+      this.loading = false
+      const homeworldRes = await fetch(person.homeworld)
+      const homeworldData = await homeworldRes.json()
+      this.homeworld = homeworldData.name
+
+      const filmsData = []
+      for (const film of person.films) {
+        const filmRes = await fetch(film)
+        const filmData = await filmRes.json()
+        filmsData.push(filmData.title)
+      }
+      this.films = filmsData
+
+      const speciesData = []
+      if (person.species.length > 0) {
+        for (const specie of person.species) {
+          const specieRes = await fetch(specie)
+          const specieData = await specieRes.json()
+          speciesData.push(specieData.name)
+        }
+        this.species = speciesData
+      } else {
+        this.species = 'Human'
+      }
+
+      const vehiclesData = []
+      if (person.vehicles.length > 0) {
+        for (const vehicle of person.vehicles) {
+          const vehicleRes = await fetch(vehicle)
+          const vehicleData = await vehicleRes.json()
+          vehiclesData.push(vehicleData.name)
+        }
+        this.vehicles = vehiclesData
+      } else {
+        this.vehicles = 'n/a'
+      }
+
+      const starshipsData = []
+      if (person.starships.length > 0) {
+        for (const starship of person.starships) {
+          const starshipRes = await fetch(starship)
+          const starshipData = await starshipRes.json()
+          starshipsData.push(starshipData.name)
+        }
+        this.starships = starshipsData
+      } else {
+        this.starships = 'n/a'
+      }
+    } catch (error) {
+      console.error(error)
+    }
   },
   methods: {
-    async fetchData(url) {
-      try {
-        this.loading = true
-        const res = await fetch(url)
-        const data = await res.json()
-        const person = data.results[0]
-        this.name = person.name
-        this.height = person.height
-        this.mass = person.mass
-        this.hairColor = person.hair_color
-        this.skinColor = person.skin_color
-        this.eyeColor = person.eye_color
-        this.birthYear = person.birth_year
-        this.gender = person.gender
-        this.loading = false
-
-        // API requests for homeworld, films, species, vehicles, and starships
-        const homeworldRes = await fetch(person.homeworld)
-        const homeworldData = await homeworldRes.json()
-        this.homeworld = homeworldData.name
-
-        const filmsData = []
-        for (const film of person.films) {
-          const filmRes = await fetch(film)
-          const filmData = await filmRes.json()
-          filmsData.push(filmData.title)
-        }
-        this.films = filmsData
-
-        const speciesData = []
-        if (person.species.length > 0) {
-          for (const specie of person.species) {
-            const specieRes = await fetch(specie)
-            const specieData = await specieRes.json()
-            speciesData.push(specieData.name)
-          }
-          this.species = speciesData
-        } else {
-          this.species = 'Human'
-        }
-
-        const vehiclesData = []
-        if (person.vehicles.length > 0) {
-          for (const vehicle of person.vehicles) {
-            const vehicleRes = await fetch(vehicle)
-            const vehicleData = await vehicleRes.json()
-            vehiclesData.push(vehicleData.name)
-          }
-          this.vehicles = vehiclesData
-        } else {
-          this.vehicles = 'n/a'
-        }
-
-        const starshipsData = []
-        if (person.starships.length > 0) {
-          for (const starship of person.starships) {
-            const starshipRes = await fetch(starship)
-            const starshipData = await starshipRes.json()
-            starshipsData.push(starshipData.name)
-          }
-          this.starships = starshipsData
-        } else {
-          this.starships = 'n/a'
-        }
-      } catch (error) {
-        console.log(error)
-        this.loading = false
-      }
+    replaceMovies(text) {
+      return text.replace(/\[|\]|"/g, '')
     },
   },
 }
