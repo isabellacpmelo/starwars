83d09ade4166b2880605d9c23ced1eaae6cc2426
isabellacpmelo <isacpmelo@gmail.com>
2023-04-19 11:43:53 -0300

    fix: Resolving error warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,10 @@ module.exports = {
   extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'unicorn/escape-case': 'off',
+    'object-shorthand': 'off',
+    'no-console': 'off',
+    'vue/no-v-html': 'off',
+  },
 }
