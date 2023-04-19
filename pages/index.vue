4f96a07025bd0fbff9791cad6c1df5d804371f2a
isabellacpmelo <isacpmelo@gmail.com>
2023-04-19 00:11:12 -0300

    fix: adjusting for loading more people to work according to scroll

--- pages/index.vue
+++ pages/index.vue
@@ -17,6 +17,7 @@
                 <PersonCard :person-name="person.name" />
               </a>
             </div>
+            <div ref="endOfList"></div>
           </div>
         </div>
       </template>
@@ -39,6 +40,7 @@ export default {
       allData: [],
       loading: false,
       page: 1,
+      reachedEnd: false,
     }
   },
   computed: {
@@ -47,28 +49,43 @@ export default {
     },
   },
   async mounted() {
+    const options = {
+      rootMargin: '0px',
+      threshold: 1.0,
+    }
+    this.observer = new IntersectionObserver(this.handleIntersection, options)
+    this.observer.observe(this.$refs.endOfList)
     await this.fetchData(`https://swapi.dev/api/people/?page=${this.page}`)
   },
-
+  beforeDestroy() {
+    if (this.observer) {
+      this.observer.disconnect()
+    }
+  },
   methods: {
     async fetchData(url) {
       try {
         this.loading = true
         const res = await fetch(url)
         const data = await res.json()
-        this.allData = this.allData.concat(data.results.slice(0, 20))
-
-        if (data.next) {
-          this.page++
-          await this.fetchData(
-            `https://swapi.dev/api/people/?page=${this.page}`
-          )
-        } else {
-          this.loading = false
+        this.allData = this.allData.concat(data.results)
+        if (!data.next) {
+          this.reachedEnd = true
+          this.observer.disconnect()
         }
+        this.loading = false
       } catch (error) {
         console.log(error)
+        this.loading = false
       }
+    },
+    handleIntersection(entries) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && !this.loading && !this.reachedEnd) {
+          this.page++
+          this.fetchData(`https://swapi.dev/api/people/?page=${this.page}`)
+        }
+      })
     },
   },
 }
