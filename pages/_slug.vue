402fb127a031895ae2c77828fd8bc01c8b883241
isabellacpmelo <isacpmelo@gmail.com>
2023-04-19 14:40:19 -0300

    feat: Adding a API requests for homeworld, films, species, vehicles, and starships #10

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -39,15 +39,20 @@
               <li>
                 <span class="font-extrabold">Homeworld:</span> {{ homeworld }}
               </li>
-              <li><span class="font-extrabold">Films: </span>{{ films }}</li>
+              <li>
+                <span class="font-extrabold">Films: </span
+                >{{ films.length ? films.join(', ') : films }}
+              </li>
               <li>
                 <span class="font-extrabold">Species: </span>{{ species }}
               </li>
               <li>
-                <span class="font-extrabold">Vehicles: </span>{{ vehicles }}
+                <span class="font-extrabold">Vehicles: </span
+                >{{ vehicles.length ? vehicles.join(', ') : vehicles }}
               </li>
               <li>
-                <span class="font-extrabold">Starships: </span>{{ starships }}
+                <span class="font-extrabold">Starships: </span
+                >{{ starships.length ? starships.join(', ') : starships }}
               </li>
             </ul>
           </div>
@@ -56,6 +61,7 @@
     </hero-section>
   </section>
 </template>
+
 <script>
 import HeroSection from '../components/HeroSection'
 
@@ -107,12 +113,56 @@ export default {
         this.eyeColor = person.eye_color
         this.birthYear = person.birth_year
         this.gender = person.gender
-        this.homeworld = person.homeworld
-        this.films = person.films
-        this.species = person.species
-        this.vehicles = person.vehicles
-        this.starships = person.starships
         this.loading = false
+
+        // API requests for homeworld, films, species, vehicles, and starships
+        const homeworldRes = await fetch(person.homeworld)
+        const homeworldData = await homeworldRes.json()
+        this.homeworld = homeworldData.name
+
+        const filmsData = []
+        for (const film of person.films) {
+          const filmRes = await fetch(film)
+          const filmData = await filmRes.json()
+          filmsData.push(filmData.title)
+        }
+        this.films = filmsData
+
+        const speciesData = []
+        if (person.species.length > 0) {
+          for (const specie of person.species) {
+            const specieRes = await fetch(specie)
+            const specieData = await specieRes.json()
+            speciesData.push(specieData.name)
+          }
+          this.species = speciesData
+        } else {
+          this.species = 'Human'
+        }
+
+        const vehiclesData = []
+        if (person.vehicles.length > 0) {
+          for (const vehicle of person.vehicles) {
+            const vehicleRes = await fetch(vehicle)
+            const vehicleData = await vehicleRes.json()
+            vehiclesData.push(vehicleData.name)
+          }
+          this.vehicles = vehiclesData
+        } else {
+          this.vehicles = 'n/a'
+        }
+
+        const starshipsData = []
+        if (person.starships.length > 0) {
+          for (const starship of person.starships) {
+            const starshipRes = await fetch(starship)
+            const starshipData = await starshipRes.json()
+            starshipsData.push(starshipData.name)
+          }
+          this.starships = starshipsData
+        } else {
+          this.starships = 'n/a'
+        }
       } catch (error) {
         console.log(error)
         this.loading = false
