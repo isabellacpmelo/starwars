83456a6b4de1913dee3b79de6b8e5981eb26f963
isabellacpmelo <isacpmelo@gmail.com>
2023-04-19 23:22:07 -0300

    fix: fixing the lister event #11

--- components/PersonCard.vue
+++ components/PersonCard.vue
@@ -2,33 +2,29 @@
   <div
     class="w-72 md:w-96 md:h-80 p-4 bg-gray-950 border border-banana-yellow rounded-lg shadow"
   >
-    <nuxt-link :to="`/${personName.replaceAll(' ', '%20')}`">
+    <nuxt-link :to="`/${person.name.replaceAll(' ', '%20')}`">
       <img
         class="rounded-lg border border-gray-900"
-        :src="`https://raw.githubusercontent.com/isabellacpmelo/starwars/main/assets/img/people/${personName.replaceAll(
+        :src="`https://raw.githubusercontent.com/isabellacpmelo/starwars/main/assets/img/people/${person.name.replaceAll(
           ' ',
           '%20'
         )}.jpeg`"
-        :alt="personName"
-        :title="personName"
+        :alt="person.name"
+        :title="person.name"
       />
     </nuxt-link>
 
     <div class="px-4 pt-4 pb-2 flex justify-between">
-      <nuxt-link :to="`/${personName.replaceAll(' ', '%20')}`">
+      <nuxt-link :to="`/${person.name.replaceAll(' ', '%20')}`">
         <h2
           class="text-center md:text-2xl text-banana-yellow font-star-wars-secondary"
         >
-          {{ personName.toLocaleLowerCase() }}
+          {{ person.name.toLocaleLowerCase() }}
         </h2>
       </nuxt-link>
       <star-favorite
         :is-favorite="isFavorite"
-        :star-color="{
-          'text-banana-yellow': isFavorite,
-          'text-gray-400': !isFavorite,
-        }"
-        @click="isFavorite = !isFavorite"
+        @toggle-favorite="toggleFavorite"
       />
     </div>
   </div>
@@ -43,15 +39,19 @@ export default {
     StarFavorite,
   },
   props: {
-    personName: {
-      type: String,
-      default: 'Luke Skywalker',
+    person: {
+      type: Object,
+      default: () => ({}),
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
     },
   },
-  data() {
-    return {
-      isFavorite: false,
-    }
+  methods: {
+    toggleFavorite() {
+      this.$emit('toggle-favorite', !this.person.isFavorite)
+    },
   },
 }
 </script>
